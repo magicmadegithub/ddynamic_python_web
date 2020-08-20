291c315ad3eabf74237067968008c4a25f4cf780
peter <maxu@100tal.com>
2020-08-20 14:17:24 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+# 静态文件目录的路径 默认当前项目中的static目录
+static_folder = 'static'
+
+# 静态文件目录的url路径 默认不写是与static_folder同名,远程静态文件时复用
+static_url_path = None,
+
+# template模板目录, 默认当前项目中的 templates 目录
+template_folder = 'templates'
+
+
+resource = {
+    "ids": [
+        {
+            "id": "homePageId001",
+            "version": "0.0.1",
+            "sdkVersion": "0.0.1",
+            "appVersion": "1.0.0",
+            "exeTime": 1597903878000,
+            "downloadUrl": "http://122.51.124.14:5000/static/resource01.zip"
+        },
+        {
+            "id": "homePageId002",
+            "version": "0.0.1",
+            "sdkVersion": "0.0.1",
+            "appVersion": "1.0.0",
+            "exeTime": 1597990278000,
+            "downloadUrl": "http://122.51.124.14:5000/static/resource02.zip"
+        },
+    ],
+    "deleteIds": []
+}
+
+
+@app.route('/dynamicResource', methods=['POST'])
+def get_resource():
+    return jsonify(resource)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == '__main__':
+    app.run()
